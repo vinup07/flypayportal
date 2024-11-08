e894f9b856755eb3b75e9b03f919aa48fd14dc9f
kaushik.udavant <kaushikudavant44@gmail.com>
2024-11-08 09:53:50 +0530

    initial commit

---
+++ src/main/java/com/flypay/flypayportal/FlypayportalApplication.java
@@ -0,0 +1,13 @@
+package com.flypay.flypayportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlypayportalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlypayportalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flypay/flypayportal/FlypayportalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flypay.flypayportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlypayportalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
