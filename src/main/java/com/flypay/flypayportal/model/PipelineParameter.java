49b5e5abdafd4c409f05a8af12f75372a83cd698
kaushik.udavant <kaushik.udavant@KTH-LAP-155.ezest.local>
2025-06-04 03:17:26 +0530

    Resolved issues

--- src/main/java/com/flypay/flypayportal/model/PipelineParameter.java
+++ src/main/java/com/flypay/flypayportal/model/PipelineParameter.java
@@ -10,7 +10,6 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 @Entity
